5d784876b1e3f4d348c2dee6f7c6c3fb72c7e7fc
saadkha5 <abzain124@gmail.com>
2025-08-11 18:01:04 +0500

    chore: add next.js config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['your-image-domain.com'],
-  },
-}
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
