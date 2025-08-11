49d6d18e8154ec7be66f41f0c32e3d3d39d60c1a
saadkha5 <abzain124@gmail.com>
2025-08-11 18:01:14 +0500

    feat: add home page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-4">Forex Portfolio Dashboard</h1>
+      <p className="text-lg">Welcome to your Forex portfolio tracker!</p>
+      {/* Add portfolio components here */}
+    </div>
+  );
+}
